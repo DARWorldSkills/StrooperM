134525f61e66d5f6cec165af721af6e7463e6ced
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-07-18 16:24:56 -0500

    commitR1

--- StrooperM/app/src/main/java/com/aprendiz/ragp/strooperm/controllers/Resumen.java
+++ StrooperM/app/src/main/java/com/aprendiz/ragp/strooperm/controllers/Resumen.java
@@ -2,14 +2,54 @@ package com.aprendiz.ragp.strooperm.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aprendiz.ragp.strooperm.R;
+import com.aprendiz.ragp.strooperm.models.GestorDB;
+import com.aprendiz.ragp.strooperm.models.Score;
 
 public class Resumen extends AppCompatActivity {
-
+    TextView txtCorrectas, txtIncorrectas, txtAciertos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen);
+        inizialite();
+        inputData();
+    }
+
+
+    private void inizialite() {
+        txtCorrectas = findViewById(R.id.txtCorrectasR);
+        txtIncorrectas = findViewById(R.id.txtIncorrectasR);
+        txtAciertos = findViewById(R.id.txtAciertosR);
+    }
+
+
+    //Método ingreso de los resultados en los campos txtCorrectas, txtIncorrectas y txtAciertos
+    private void inputData() {
+        if (MenuJ.guardar==1){
+            txtCorrectas.setText(Integer.toString(Juego.correctas));
+            txtIncorrectas.setText(Integer.toString(Juego.incorrectas));
+            txtAciertos.setText(Integer.toString(Juego.aciertos));
+            inputInDataBase();
+
+        }else {
+            txtCorrectas.setText(Integer.toString(JuegoC.correctas));
+            txtIncorrectas.setText(Integer.toString(JuegoC.incorrectas));
+            txtAciertos.setText(Integer.toString(JuegoC.aciertos));
+        }
+
+    }
+
+    //Método para el llamado del método inputData de la Clase GestorDB
+    private void inputInDataBase() {
+        Score score = new Score();
+        score.setPuntuacion(Integer.toString(Juego.intentos));
+        score.setIncorrectas(Integer.toString(Juego.incorrectas));
+        GestorDB gestorDB = new GestorDB(this);
+        gestorDB.inputData(score);
+
     }
 }
--- StrooperM/app/src/main/java/com/aprendiz/ragp/strooperm/models/GestorDB.java
+++ StrooperM/app/src/main/java/com/aprendiz/ragp/strooperm/models/GestorDB.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +48,7 @@ public class GestorDB extends SQLiteOpenHelper {
         }
         cursor.close();
         db.close();
+
 
         return results;
     }
